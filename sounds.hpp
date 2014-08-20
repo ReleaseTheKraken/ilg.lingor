2c3f390a001795472cc92af0807b15cd457d29f8
Infectiondg <linusg@hotmail.se>
2014-08-20 22:02:06 +0200

    Sound file update
    For coast guard chopper

--- sounds.hpp
+++ sounds.hpp
@@ -2,9 +2,9 @@ class Siren_Short
 
 {
 
-name     = "";	
-sound[]  = {"\Siren_Short.ogg", 1, 1};
-titles[] = {};
+	name     = "";	
+	sound[]  = {"\Siren_Short.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -12,9 +12,9 @@ class Siren_Long
 
 {
 
-name     = "";
-sound[]  = {"\Siren_Long.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\Siren_Long.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -22,9 +22,9 @@ class call911
 
 {
 
-name     = "";
-sound[]  = {"\snd\911call.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\snd\911call.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -32,9 +32,9 @@ class bankalarm
 
 {
 
-name     = "";
-sound[]  = {"\snd\bankalarm.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\snd\bankalarm.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -42,9 +42,9 @@ class accessgranted
 
 {
 
-name     = "";
-sound[]  = {"policePC\sound\accessgranted.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"policePC\sound\accessgranted.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -52,9 +52,9 @@ class accessdenied
 
 {
 
-name     = "";
-sound[]  = {"policePC\sound\accessdenied.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"policePC\sound\accessdenied.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -62,9 +62,9 @@ class beepsimple
 
 {
 
-name     = "";
-sound[]  = {"\snd\simplebeep.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\snd\simplebeep.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -72,9 +72,9 @@ class alarmbell
 
 {
 
-name     = "";
-sound[]  = {"\snd\alarmbell.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\snd\alarmbell.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -82,9 +82,9 @@ class alarm4
 
 {
 
-name     = "";
-sound[]  = {"\alarm4.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\alarm4.ogg", 1, 1};
+	titles[] = {};
 
 };
 
@@ -92,9 +92,9 @@ class Forest_fire
 
 {
 
-name     = "";
-sound[]  = {"\snd\ForestFire1.ogg", 4, 2};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\snd\ForestFire1.ogg", 4, 2};
+	titles[] = {};
 
 };
 
@@ -102,9 +102,9 @@ class Flash_Bang
 
 {
 
-name     = "";
-sound[]  = {"\snd\Flashbang.ogg", 4, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\snd\Flashbang.ogg", 4, 1};
+	titles[] = {};
 
 };
 
@@ -112,40 +112,94 @@ class netcops
 
 {
 
-name     = "";
-sound[]  = {"\snd\netcops.ogg", 1, 1};
-titles[] = {};
+	name     = "";
+	sound[]  = {"\snd\netcops.ogg", 1, 1};
+	titles[] = {};
 
 };
 
 class iramusic
 {
-name = "IRAsong"; // Name for mission editor
-sound[] = {"\snd\IRAsong.ogg", 1, 1};
-titles[] = {};
+	name = "IRAsong"; // Name for mission editor
+	sound[] = {"\snd\IRAsong.ogg", 1, 1};
+	titles[] = {};
 };
 
 class redarmymusic
 {
-name = "redarmy"; // Name for mission editor
-sound[] = {"\snd\redarmy.ogg", 1, 1};
-titles[] = {};
+	name = "redarmy"; // Name for mission editor
+	sound[] = {"\snd\redarmy.ogg", 1, 1};
+	titles[] = {};
 };
- 
-   class DuctTapeStrap
-  {
-   name = "";
-   sound[] = {"\snd\DuctTapeStrap.ogg", 1, 1};
-   titles[] = {};
-  };
 
-   class Unlock : DuctTapeStrap {sound[] = {"\snd\Unlock.ogg", db+11, 1};}; 
-   class Scroll : DuctTapeStrap {sound[] = {"\snd\Scroll.ogg", db-10, 1};}; 
-   class Thingie : DuctTapeStrap {sound[] = {"\snd\Thingie.ogg", 1, 1};}; 
+class DuctTapeStrap
+{
+	name = "";
+	sound[] = {"\snd\DuctTapeStrap.ogg", 1, 1};
+	titles[] = {};
+};
+
+class Unlock : DuctTapeStrap {sound[] = {"\snd\Unlock.ogg", db+11, 1};}; 
+class Scroll : DuctTapeStrap {sound[] = {"\snd\Scroll.ogg", db-10, 1};}; 
+class Thingie : DuctTapeStrap {sound[] = {"\snd\Thingie.ogg", 1, 1};}; 
 
 class carallarm
-	{
-		name = "carallarm"; // Name for mission editor
-		sound[] = {\snd\carallarm.ogg, 4, 1};
-		titles[] = {0, ""};
-	};
+{
+	name = "carallarm"; // Name for mission editor
+	sound[] = {\snd\carallarm.ogg, 4, 1};
+	titles[] = {0, ""};
+};
+class intercom1
+{
+	name = "intercom1";   
+	sound[] = {"sounds\R_BasketAtCabin.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
+class intercom2
+{
+	name = "intercom2";   // how the sound is referred to in the editor (e.g. trigger effects)
+	sound[] = {"sounds\R_BasketGoing.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
+class intercom3
+{
+	name = "intercom3";   // how the sound is referred to in the editor (e.g. trigger effects)
+	sound[] = {"sounds\R_BasketReadyPick.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
+class intercom4
+{
+	name = "intercom4";   // how the sound is referred to in the editor (e.g. trigger effects)
+	sound[] = {"sounds\R_SurvivorIn.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
+class intercom5
+{
+	name = "intercom5";   // how the sound is referred to in the editor (e.g. trigger effects)
+	sound[] = {"sounds\R_SurvivorComing.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
+class intercom6
+{
+	name = "intercom6";   // how the sound is referred to in the editor (e.g. trigger effects)
+	sound[] = {"sounds\R_SurvivorAtCabin.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
+class intercom7
+{
+	name = "intercom7";   // how the sound is referred to in the editor (e.g. trigger effects)
+	sound[] = {"sounds\R_SurvivorIn.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
+class intercom8
+{
+	name = "intercom8";   // how the sound is referred to in the editor (e.g. trigger effects)
+	sound[] = {"sounds\R_BasketInCabin.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
+class intercom9
+{
+	name = "intercom9";   // how the sound is referred to in the editor (e.g. trigger effects)
+	sound[] = {"sounds\R_BasketComing.ogg", 1, 1};   // filename, volume, pitch
+	titles[] = {};
+};
